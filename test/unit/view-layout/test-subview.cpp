34cca1a2175f572b3339ac9fad358bc76a4a0d7a
gberg617 <guybergel@gmail.com>
2025-10-14 14:55:52 -0700

    started implementing SubLayout

--- test/unit/view-layout/test-subview.cpp
+++ test/unit/view-layout/test-subview.cpp
@@ -76,6 +76,8 @@ TEST(SubView, FixedFirstDimSubView2D)
     EXPECT_EQ(sv(1), 5);
     EXPECT_EQ(sv(2), 6);
 
+    EXPECT_EQ(sv.get_sublayout().size(), 3);
+
 }
 
 TEST(SubView, RangeFirstDimSubView2D)
@@ -96,7 +98,9 @@ TEST(SubView, RangeFirstDimSubView2D)
     EXPECT_EQ(sv(1,1), 8);
     EXPECT_EQ(sv(1,2), 9);
 
-    sv.set_slice<0>(RangeSlice<>{0,1});
+    EXPECT_EQ(sv.get_sublayout().size(), 6);
+
+    sv.get_sublayout().set_slice<0>(RangeSlice<>{0,1});
 
     EXPECT_EQ(sv(0,0), 1);
     EXPECT_EQ(sv(0,1), 2);
@@ -106,9 +110,11 @@ TEST(SubView, RangeFirstDimSubView2D)
     EXPECT_EQ(sv(1,1), 5);
     EXPECT_EQ(sv(1,2), 6);
 
+    EXPECT_EQ(sv.get_sublayout().size(), 6);
+
 }
 
-// To Do tests: subview of subview, "sliding winow" modify slice entries, GPU tests.
+// To Do tests: subview of subview
 
 // void test_subviewGPU() {
 // #if defined(RAJA_ENABLE_HIP)
