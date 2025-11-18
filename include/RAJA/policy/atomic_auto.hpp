bdfcdce7512d203c6fa4b82992e5c1e4c3904cbc
Steven Smith <smith84@llnl.gov>
2025-11-17 16:58:56 -0800

    Fix SYCL compile issue with downstream builds that are not using SYCL

--- include/RAJA/policy/atomic_auto.hpp
+++ include/RAJA/policy/atomic_auto.hpp
@@ -44,7 +44,7 @@
 #elif defined(__HIP_DEVICE_COMPILE__) && defined(RAJA_HIP_ACTIVE)
 #define RAJA_AUTO_ATOMIC                                                       \
   RAJA::hip_atomic {}
-#elif defined(__SYCL_DEVICE_ONLY__)
+#elif defined(__SYCL_DEVICE_ONLY__) && defined(RAJA_SYCL_ACTIVE)
 #define RAJA_AUTO_ATOMIC                                                       \
   RAJA::sycl_atomic {}
 #elif defined(RAJA_OPENMP_ACTIVE)
@@ -54,7 +54,6 @@
 #define RAJA_AUTO_ATOMIC                                                       \
   RAJA::seq_atomic {}
 #endif
-
 
 namespace RAJA
 {
