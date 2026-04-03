6c54ad7f13decedf32b22e9f561622f3bc344e0d
john bowen <john.bowen42@gmail.com>
2026-04-03 11:36:57 -0700

    Fix sycl scripts and add back cmake versioning logic

--- host-configs/lc-builds/toss4/corona_sycl.cmake
+++ host-configs/lc-builds/toss4/corona_sycl.cmake
@@ -12,8 +12,8 @@ set(RAJA_COMPILER "RAJA_COMPILER_CLANG" CACHE STRING "")
 set(CMAKE_CXX_COMPILER "clang++" CACHE PATH "")
 #set(CMAKE_CXX_COMPILER "dpcpp" CACHE PATH "")
 
-set(CMAKE_CXX_FLAGS_RELEASE "-O3 -std=c++20 -L${SYCL_LIB_PATH} -fsycl -fsycl-unnamed-lambda -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906" CACHE STRING "")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -std=c++20 -g -L${SYCL_LIB_PATH} -fsycl -fsycl-unnamed-lambda -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906" CACHE STRING "")
-set(CMAKE_CXX_FLAGS_DEBUG "-O0 -std=c++20 -g -L${SYCL_LIB_PATH} -fsycl -fsycl-unnamed-lambda -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -L${SYCL_LIB_PATH} -fsycl -fsycl-unnamed-lambda -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O3 -g -L${SYCL_LIB_PATH} -fsycl -fsycl-unnamed-lambda -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g -L${SYCL_LIB_PATH} -fsycl -fsycl-unnamed-lambda -fsycl-targets=amdgcn-amd-amdhsa -Xsycl-target-backend --offload-arch=gfx906" CACHE STRING "")
 
 set(RAJA_HOST_CONFIG_LOADED On CACHE BOOL "")
