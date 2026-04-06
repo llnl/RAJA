4d809f2a2d715dca9b4a345d87045385b59d6d43
Brandon Echols <echols2@llnl.gov>
2026-04-06 14:01:49 -0700

    Fixes format

--- include/RAJA/util/FunctionTypeTraits.hpp
+++ include/RAJA/util/FunctionTypeTraits.hpp
@@ -31,33 +31,34 @@ template<typename Fn>
 struct signature_impl;
 
 template<typename C, typename R, typename... Args>
-struct signature_impl<R (C::*) (Args...)>
+struct signature_impl<R (C::*)(Args...)>
 {
   using type   = R(Args...);
   using args_t = camp::tuple<Args...>;
 };
 
 template<typename C, typename R, typename... Args>
-struct signature_impl<R (C::*) (Args...) const> : public signature_impl<R(C::*)(Args...)>
-{
-};
+struct signature_impl<R (C::*)(Args...) const>
+    : public signature_impl<R (C::*)(Args...)>
+{};
 
 template<typename C, typename R, typename... Args>
-struct signature_impl<R(C::*)(Args...) noexcept> : public signature_impl<R(C::*)(Args...)>
-{
-};
+struct signature_impl<R (C::*)(Args...) noexcept>
+    : public signature_impl<R (C::*)(Args...)>
+{};
 
 template<typename C, typename R, typename... Args>
-struct signature_impl<R(C::*)(Args...) const noexcept> : public signature_impl<R(C::*)(Args...)>
-{
-};
+struct signature_impl<R (C::*)(Args...) const noexcept>
+    : public signature_impl<R (C::*)(Args...)>
+{};
 
-template <typename T>
+template<typename T>
 using signature_t = typename signature_impl<T>::type;
 
-template <std::size_t ArgNum, typename T>
-using func_arg_t = camp::tuple_element_t<ArgNum, typename signature_impl<T>::args_t>;
+template<std::size_t ArgNum, typename T>
+using func_arg_t =
+    camp::tuple_element_t<ArgNum, typename signature_impl<T>::args_t>;
 
 }  // namespace internal
 }  // namespace RAJA
-#endif // RAJA_FUNCTION_SIGNATURE_UTILS_HPP
+#endif  // RAJA_FUNCTION_SIGNATURE_UTILS_HPP
