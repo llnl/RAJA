947abb20ebf2cf12f2b87e0255254dd0cfcca097
Alan Dayton <dayton8@llnl.gov>
2026-06-01 09:21:57 -0700

    Revert trivially copyable requirement

--- include/RAJA/util/TypeConvert.hpp
+++ include/RAJA/util/TypeConvert.hpp
@@ -50,8 +50,7 @@ template<typename A, typename B>
 RAJA_INLINE RAJA_HOST_DEVICE constexpr B reinterp_A_as_B(A const& a)
 {
   static_assert(sizeof(A) == sizeof(B), "A and B must be the same size");
-  static_assert(std::is_trivially_copyable_v<A>, "A must be trivially copyable");
-  static_assert(std::is_trivially_copyable_v<B>, "B must be trivially copyable");
+  // TODO: Consider requiring A and B to be trivially copyable
 
   B b;
   std::memcpy(&b, &a, sizeof(A));
