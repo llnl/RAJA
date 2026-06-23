050ca6b56044a04ef35bb9029212eaae9a7f8c5a
Robert Chen <chen59@llnl.gov>
2026-06-23 09:06:54 -0700

    Always get the dim size from the lhs, according to the dim passed in.

--- include/RAJA/pattern/tensor/internal/ET/BinaryOperatorTraits.hpp
+++ include/RAJA/pattern/tensor/internal/ET/BinaryOperatorTraits.hpp
@@ -101,7 +101,7 @@ struct OperatorTraits
   RAJA_HOST_DEVICE
   static int getDimSize(int dim, LHS_TYPE const& lhs, RHS_TYPE const& rhs)
   {
-    return dim == 0 ? lhs.getDimSize(0) : rhs.getDimSize(1);
+    return lhs.getDimSize(dim);
   }
 };
 
