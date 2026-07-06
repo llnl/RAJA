0135fc33d5e012246947fdb093534b24adde40ca
Brandon Echols <echols2@llnl.gov>
2026-07-06 11:30:54 -0700

    Updates CAS loop to use generic atomic in RAJA
      - Gives more descriptive name for container type
      - Makes explicit ctor

--- include/RAJA/policy/msg_queue/mpsc_queue.hpp
+++ include/RAJA/policy/msg_queue/mpsc_queue.hpp
@@ -35,25 +35,25 @@ namespace RAJA
 namespace messages
 {
 
-template<typename Container, typename... Args>
-class Queue<Container, RAJA::mpsc_queue, RAJA::MsgArgs<Args...>>
+template<typename MsgBusType, typename... Args>
+class Queue<MsgBusType, RAJA::mpsc_queue, RAJA::MsgArgs<Args...>>
 {
 public:
   using policy = RAJA::mpsc_queue;
 
   using args_type = camp::tuple<Args...>;
-  using size_type = typename Container::size_type;
+  using size_type = typename MsgBusType::size_type;
 
-  Queue(std::pair<std::size_t, std::size_t> id, Container& container)
+  explicit Queue(std::pair<std::size_t, std::size_t> id, MsgBusType& bus)
       : m_type {id.first},
         m_hash {id.second},
-        m_container {&container}
+        m_bus {&bus}
   {}
 
-  Queue(std::pair<std::size_t, std::size_t> id, Container* container)
+  explicit Queue(std::pair<std::size_t, std::size_t> id, MsgBusType* bus)
       : m_type {id.first},
         m_hash {id.second},
-        m_container {container}
+        m_bus {bus}
   {}
 
   auto get_id() const noexcept { return std::make_pair(m_type, m_hash); }
@@ -64,36 +64,27 @@ public:
   template<typename... Ts>
   RAJA_HOST_DEVICE bool try_post_message(Ts&&... args) const
   {
-    if (m_container != nullptr)
+    if (m_bus != nullptr)
     {
       constexpr size_type header_sz = align_sz(sizeof(MsgHeader));
       constexpr size_type args_sz   = align_sz(sizeof(MsgArgs<Args...>));
       constexpr size_type msg_sz    = header_sz + args_sz;
 
-      size_type local_sz;
-      size_type old_sz = RAJA::atomicLoad<auto_atomic>(&(m_container->m_end));
+      const size_type capacity = m_bus->m_capacity;
 
       // Checks if message can fit in queue. If so, adds msg_sz to end of queue
       // to reserve space. Otherwise, message doesn't fit and no space is
       // reserved. In other words, the CAS-loop below performs the follwing
       // operation:
       // (*address + msg_sz <= capacity) ?  (*address + msg_sz) : *address;
-      do
-      {
-        size_type new_sz = old_sz + msg_sz;
-        local_sz         = old_sz;  // offset to start of message
-        // Checks if fits in queue
-        if (new_sz <= m_container->m_capacity)
-        {
-          old_sz = RAJA::atomicCAS<auto_atomic>(&(m_container->m_end), local_sz,
-                                                new_sz);
-        }
-      } while (local_sz != old_sz);
+      size_type local_sz = RAJA::atomicGeneric<auto_atomic>(
+          &(m_bus->m_end), [=](size_type old_sz) {
+            return (old_sz + msg_sz <= capacity) ? (old_sz + msg_sz) : old_sz;
+          });
 
-      if (m_container->m_data != nullptr &&
-          local_sz + msg_sz <= m_container->m_capacity)
+      if (m_bus->m_data != nullptr && local_sz + msg_sz <= capacity)
       {
-        char* buf = m_container->m_data + local_sz;
+        char* buf = m_bus->m_data + local_sz;
         new (buf) MsgHeader {args_sz, m_type, m_hash, buf + header_sz};
         new (buf + header_sz)
             MsgArgs<Args...> {args_type(std::forward<Ts>(args)...)};
@@ -108,7 +99,7 @@ public:
 private:
   std::size_t m_type;
   std::size_t m_hash;
-  Container* m_container;
+  MsgBusType* m_bus;
 };
 
 }  // namespace messages
--- include/RAJA/policy/msg_queue/spsc_queue.hpp
+++ include/RAJA/policy/msg_queue/spsc_queue.hpp
@@ -34,46 +34,46 @@ namespace RAJA
 namespace messages
 {
 
-template<typename Container, typename... Args>
-class Queue<Container, RAJA::spsc_queue, RAJA::MsgArgs<Args...>>
+template<typename MsgBusType, typename... Args>
+class Queue<MsgBusType, RAJA::spsc_queue, RAJA::MsgArgs<Args...>>
 {
 public:
   using policy = RAJA::spsc_queue;
 
   using args_type = camp::tuple<Args...>;
-  using size_type = typename Container::size_type;
+  using size_type = typename MsgBusType::size_type;
 
-  Queue(std::pair<std::size_t, std::size_t> id, Container& container)
+  explicit Queue(std::pair<std::size_t, std::size_t> id, MsgBusType& bus)
       : m_type {id.first},
         m_hash {id.second},
-        m_container {&container}
+        m_bus {&bus}
   {}
 
-  Queue(std::pair<std::size_t, std::size_t> id, Container* container)
+  explicit Queue(std::pair<std::size_t, std::size_t> id, MsgBusType* bus)
       : m_type {id.first},
         m_hash {id.second},
-        m_container {container}
+        m_bus {bus}
   {}
 
   auto get_id() const noexcept { return std::make_pair(m_type, m_hash); }
 
   /// Posts message to queue. This is marked `const` to pass to lambda by
   /// copy. This throws away messages that are over the capacity of the
-  /// container.
+  /// bus.
   template<typename... Ts>
   bool try_post_message(Ts&&... args) const
   {
-    if (m_container != nullptr)
+    if (m_bus != nullptr)
     {
       constexpr size_type header_sz = align_sz(sizeof(MsgHeader));
       constexpr size_type args_sz   = align_sz(sizeof(MsgArgs<Args...>));
       constexpr size_type msg_sz    = header_sz + args_sz;
-      auto local_size               = m_container->m_end;
-      if (m_container->m_data != nullptr &&
-          local_size + msg_sz <= m_container->m_capacity)
+
+      auto local_size = m_bus->m_end;
+      if (m_bus->m_data != nullptr && local_size + msg_sz <= m_bus->m_capacity)
       {
-        m_container->m_end += msg_sz;
-        char* buf = m_container->m_data + local_size;
+        m_bus->m_end += msg_sz;
+        char* buf = m_bus->m_data + local_size;
         new (buf) MsgHeader {args_sz, m_type, m_hash, buf + header_sz};
         new (buf + header_sz)
             MsgArgs<Args...> {args_type(std::forward<Ts>(args)...)};
@@ -88,7 +88,7 @@ public:
 private:
   std::size_t m_type;
   std::size_t m_hash;
-  Container* m_container;
+  MsgBusType* m_bus;
 };
 
 }  // namespace messages
