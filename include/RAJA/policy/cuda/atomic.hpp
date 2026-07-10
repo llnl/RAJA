f73fa101aab7f606198f988e9351415108213c70
Alan Dayton <dayton8@llnl.gov>
2026-07-10 09:43:21 -0700

    Use cuda::atomic_ref for load and store

--- include/RAJA/policy/cuda/atomic.hpp
+++ include/RAJA/policy/cuda/atomic.hpp
@@ -28,11 +28,6 @@
 #include <type_traits>
 #include <utility>
 
-#if __CUDA__ARCH__ >= 600 && __CUDACC_VER_MAJOR__ >= 11 &&                     \
-    __CUDACC_VER_MINOR__ >= 6
-#define RAJA_ENABLE_CUDA_ATOMIC_REF
-#endif
-
 #include <cuda/atomic>
 
 #include "camp/list.hpp"
@@ -179,10 +174,8 @@ RAJA_INLINE __device__ T cuda_atomicExchange(T* acc, T value)
 }
 
 /*!
- * Atomic load and store
+ * Atomic load
  */
-#if defined(RAJA_ENABLE_CUDA_ATOMIC_REF)
-
 template<typename T>
 RAJA_INLINE __device__ T cuda_atomicLoad(T* acc)
 {
@@ -190,40 +183,15 @@ RAJA_INLINE __device__ T cuda_atomicLoad(T* acc)
       cuda::memory_order_relaxed {});
 }
 
+/*!
+ * Atomic store
+ */
 template<typename T>
 RAJA_INLINE __device__ void cuda_atomicStore(T* acc, T value)
 {
   cuda::atomic_ref<T, cuda::thread_scope_device>(*acc).store(
       value, cuda::memory_order_relaxed {});
 }
-
-#else
-
-template<typename T,
-         std::enable_if_t<cuda_useBuiltinCommon<T>::value, bool> = true>
-RAJA_INLINE __device__ T cuda_atomicLoad(T* acc)
-{
-  return cuda_atomicOr(acc, static_cast<T>(0));
-}
-
-template<typename T,
-         std::enable_if_t<cuda_useReinterpretCommon<T>::value, bool> = true>
-RAJA_INLINE __device__ T cuda_atomicLoad(T* acc)
-{
-  using R = cuda_useReinterpretCommon_t<T>;
-
-  return RAJA::util::reinterp_A_as_B<R, T>(
-      cuda_atomicLoad(reinterpret_cast<R*>(acc)));
-}
-
-template<typename T>
-RAJA_INLINE __device__ void cuda_atomicStore(T* acc, T value)
-{
-  cuda_atomicExchange(acc, value);
-}
-
-#endif
-
 
 /*!
  * Atomic compare and swap
