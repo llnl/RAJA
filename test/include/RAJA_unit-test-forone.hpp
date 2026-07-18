e785d7fccdc51ad82f0d84ce13077cc0ddfbcf53
Jason Burmark <burmark1@llnl.gov>
2026-07-17 17:04:49 -0700

    Add omp, sycl forone, update ompt forone

--- test/include/RAJA_unit-test-forone.hpp
+++ test/include/RAJA_unit-test-forone.hpp
@@ -30,14 +30,90 @@ inline void forone(test_seq, L&& run)
   std::forward<L>(run)();
 }
 
+#if defined(RAJA_ENABLE_OPENMP)
+
+// test_openmp implementation
+template < typename L >
+inline void forone(test_openmp, L&& run)
+{
+  using BODY = camp::decay<L>;
+  BODY input_body(std::forward<L>(run));
+
+#pragma omp parallel num_threads(1)
+  {
+    auto body = input_body;
+#pragma omp for
+    for (int i = 0; i < 1; ++i) {
+      RAJA_UNUSED_VAR(i);
+      body();
+    }
+  }
+}
+
+#endif
+
 #if defined(RAJA_ENABLE_TARGET_OPENMP)
 
 // test_openmp_target implementation
 template < typename L >
 inline void forone(test_openmp_target, L&& run)
 {
-#pragma omp target
-  run();
+  using BODY = camp::decay<L>;
+  BODY input_body(std::forward<L>(run));
+
+#pragma omp target parallel for \
+        map(to : input_body)
+  for (int i = 0; i < 1; ++i) {
+    RAJA_UNUSED_VAR(i);
+    auto body = input_body;
+    body();
+  }
+}
+
+#endif
+
+#if defined(RAJA_ENABLE_SYCL)
+
+// test_sycl implementation
+template < typename L >
+inline void forone(test_sycl, L&& run)
+{
+  using BODY = camp::decay<L>;
+
+  auto sycl_res = RAJA::resources::Sycl::get_default();
+  ::sycl::queue* q = sycl_res.get_queue();
+  BODY input_body(std::forward<L>(run));
+
+  BODY* device_body = nullptr;
+  if constexpr (!std::is_trivially_copyable_v<BODY>) {
+    device_body = static_cast<BODY*>(::sycl::malloc_device(sizeof(BODY), *q));
+    q->memcpy(device_body, &input_body, sizeof(BODY)).wait();
+  }
+
+  ::sycl::range<1> grid(1);
+  ::sycl::range<1> block(1);
+  q->submit([&](::sycl::handler& h) {
+    if constexpr (std::is_trivially_copyable_v<BODY>) {
+      h.parallel_for(::sycl::nd_range<1>{grid, block}, [=](::sycl::nd_item<1> item) {
+        auto body = input_body;
+        if (item.get_global_id(0) == 0) {
+          body();
+        }
+      });
+    } else {
+      h.parallel_for(::sycl::nd_range<1>{grid, block}, [=](::sycl::nd_item<1> item) {
+        auto body = *device_body;
+        if (item.get_global_id(0) == 0) {
+          body();
+        }
+      });
+    }
+  });
+  q->wait();
+
+  if constexpr (!std::is_trivially_copyable_v<BODY>) {
+    ::sycl::free(device_body, *q);
+  }
 }
 
 #endif
