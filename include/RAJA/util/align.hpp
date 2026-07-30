64e0148b15fc9b18acd4a1935e27acdd64ce100d
Brandon Echols <echols2@llnl.gov>
2026-07-30 10:06:14 -0700

    Uses DATA_ALIGN for alignment size in messages

--- include/RAJA/util/align.hpp
+++ include/RAJA/util/align.hpp
@@ -28,7 +28,7 @@
 namespace RAJA
 {
 RAJA_HOST_DEVICE
-constexpr std::size_t align_sz(std::size_t size, std::size_t alignment = 16)
+constexpr std::size_t align_sz(std::size_t size, std::size_t alignment = RAJA::DATA_ALIGN)
 /** Returns the aligned size. This would use `alignof(std::max_align_t)`;
  *  however, the device side can get a different value compared to the
  *  host.
