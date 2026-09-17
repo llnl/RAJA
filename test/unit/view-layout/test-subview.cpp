6e9e906756fd7741afc9455749306e5eeaa55b91
gberg617 <guybergel@gmail.com>
2026-09-17 12:48:35 -0700

    replace camp->RAJA in copyright statement

--- test/unit/view-layout/test-subview.cpp
+++ test/unit/view-layout/test-subview.cpp
@@ -1,8 +1,8 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 // Copyright (c) Lawrence Livermore National Security, LLC and other
-// Camp Project Developers. See top-level LICENSE and COPYRIGHT
+// RAJA Project Developers. See top-level LICENSE and COPYRIGHT
 // files for dates and other details. No copyright assignment is required
-// to contribute to Camp.
+// to contribute to RAJA.
 //
 // SPDX-License-Identifier: (BSD-3-Clause)
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
